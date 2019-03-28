c49f650ae2e6fa90778035f219182e465dc9637a
Anthony chaussin <43750490+anthonychaussin@users.noreply.github.com>
2019-03-28 20:06:52 +0100

    Update Db.php

--- app/Db.php
+++ app/Db.php
@@ -9,11 +9,11 @@ class Db extends Model
 	function __construct()
 	{
 		switch ($_SERVER['HTTP_HOST']) {
-			case "10.103.1.202":
-				$this->dbString = new PDO("mysql:host=localhost;dbname=huitahuit", "huitahuit", "groupe1");
+			case "*******":
+				$this->dbString = new PDO("mysql:host=localhost;dbname=*******", "******", "******");
 				break;
-			case "spar1950g1.000webhostapp.com":
-				$this->dbString = new PDO("mysql:host=localhost;dbname=id7005313_spar", "id7005313_groupe1", "groupe1");
+			case "*********":
+				$this->dbString = new PDO("mysql:host=localhost;dbname=*******", "*******", "*******");
 				break;
 			default:
 				throw new Exception("Erreur ce serveur est inconu et/ou n'est lier à aucune base de donnée", 1);
